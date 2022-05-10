47e83fd960a2a2438c1956e045d0401a41d009db
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2022-05-10 19:49:04 +0400

    Finish Flight Reservation Django Rest Framework and Angular

--- flighReservationAngular/src/app/components/passenger-details/passenger-details.component.ts
+++ flighReservationAngular/src/app/components/passenger-details/passenger-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute,Router } from '@angular/router';
+import { Reservation } from 'src/app/model/reservation';
 import { ReservationService } from 'src/app/services/reservation.service';
 
 @Component({
@@ -9,12 +10,20 @@ import { ReservationService } from 'src/app/services/reservation.service';
 })
 export class PassengerDetailsComponent implements OnInit {
   public flightData:any;
-
+  reservation:Reservation = new Reservation('','','','','','','','','')
   constructor(private service:ReservationService,private router:Router,private route:ActivatedRoute){}
 
   ngOnInit(): void {
-    this.service.getFlights(Number.parseInt(this.route.snapshot.paramMap.get('id') as string)).subscribe((response:any)=>{
+    this.service.getFlights(Number.parseInt(this.route.snapshot.paramMap.get('id') as any)).subscribe((response:any)=>{
       this.flightData = response
+    })
+  }
+
+  public onSubmit(){
+    this.reservation.flightId = this.flightData.id
+    console.log(this.flightData.id)
+    this.service.saveReservation(this.reservation).subscribe((response:any)=>{
+      this.router.navigate(['/confirm',response.id])
     })
   }
 
